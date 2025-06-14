54298f28f3fecb3e2ad7a1c110e62fd650e8b784
climbedtower <hanami.ippai@gmail.com>
2025-06-15 01:07:14 +0900

    Add dynamic menu generation using LinkManager

--- topDoor/topDoorApp.swift
+++ topDoor/topDoorApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Combine
 
 @main
 struct MenuLinkerApp: App {
@@ -13,34 +14,47 @@ struct MenuLinkerApp: App {
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem?
+    var linkManager = LinkManager()
+    var linksSubscriber: AnyCancellable?
     
     func applicationDidFinishLaunching(_ notification: Notification) {
         // メニューバーアイテムを作成
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        
+
         if let button = statusItem?.button {
             button.image = NSImage(systemSymbolName: "link", accessibilityDescription: "Links")
         }
-        
-        // 仮のメニューを作成
-        let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Google", action: #selector(openGoogle), keyEquivalent: ""))
-        menu.addItem(NSMenuItem(title: "Apple", action: #selector(openApple), keyEquivalent: ""))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
-        
-        statusItem?.menu = menu
-    }
-    
-    @objc func openGoogle() {
-        if let url = URL(string: "https://www.google.com") {
-            NSWorkspace.shared.open(url)
+
+        buildMenu()
+
+        // リンク変更を監視し、メニューを更新
+        linksSubscriber = linkManager.$links.sink { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.buildMenu()
+            }
         }
     }
-    
-    @objc func openApple() {
-        if let url = URL(string: "https://www.apple.com") {
-        NSWorkspace.shared.open(url)
+
+    func buildMenu() {
+        let menu = NSMenu()
+
+        // 動的にリンクメニューを生成
+        for link in linkManager.links {
+            let item = NSMenuItem(title: link.name, action: #selector(openLinkItem(_:)), keyEquivalent: "")
+            item.representedObject = link
+            item.target = self
+            menu.addItem(item)
+        }
+
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
+
+        statusItem?.menu = menu
+    }
+
+    @objc func openLinkItem(_ sender: NSMenuItem) {
+        if let link = sender.representedObject as? LinkItem {
+            linkManager.openLink(link)
         }
     }
     
