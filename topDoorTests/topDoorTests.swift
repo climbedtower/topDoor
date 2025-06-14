3fdad39aa0eab974a263a101d585abb32e643412
climbedtower <hanami.ippai@gmail.com>
2025-06-15 00:23:29 +0900

    Initial Commit

---
+++ topDoorTests/topDoorTests.swift
@@ -0,0 +1,16 @@
+//
+//  topDoorTests.swift
+//  topDoorTests
+//
+//  Created by yuco on 2025/06/15.
+//
+
+import Testing
+
+struct topDoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
