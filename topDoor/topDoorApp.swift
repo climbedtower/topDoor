5f6341c000a5b5d2724bc04bb5bb47c219b700b8
climbedtower <hanami.ippai@gmail.com>
2025-06-15 05:46:50 +0900

    Initial Commit

---
+++ topDoor/topDoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  topDoorApp.swift
+//  topDoor
+//
+//  Created by yuco on 2025/06/15.
+//
+
+import SwiftUI
+
+@main
+struct topDoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
